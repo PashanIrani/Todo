87aaf4d6c1100e8dd470319b6b9bd474a3821028
Pashan Irani <pashanirani@gmail.com>
2017-10-28 10:58:43 -0700

    started implementation

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -8,6 +8,20 @@ $sql = "SELECT * FROM user WHERE email";
 
 $result = mysqli_query($dbc, $sql);
 
-//TODO: implement login
+$sql = $dbc->prepare("SELECT password FROM user WHERE email=?");
+$sql->bind_param("s", $email);
+$sql->execute();
 
+$sql->store_result();
+$sql->bind_result($column1);
+$stored_password = $column1;
+
+while ($sql->fetch()) {
+    $stored_password = $column1;
+}
+
+header("HTTP/1.0 422 " . $stored_password);
+
+
+$sql->close();
 $dbc->close();
