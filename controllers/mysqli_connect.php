8ff249317fc74d797b0ce582911f2866f43d841a
Pashan Irani <pashanirani@gmail.com>
2017-11-13 08:08:11 -0800

    back to local, will have to configure app in PDO for heroku

--- controllers/mysqli_connect.php
+++ controllers/mysqli_connect.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_USER', 'softccfpsvygki');
-define('DB_PASS', '0c066f0ddb19e697a3dc555eb1a6f11fcce32e6df8e6ce6ea05e2379b849a57c');
-define('DB_HOST', 'ec2-107-22-162-82.compute-1.amazonaws.com');
-define('DB_NAME', 'debed0decaf41g');
+define('DB_USER', 'user');
+define('DB_PASS', 'password');
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'todo');
 
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
