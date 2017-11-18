6df3f03642e241657aecd76c2b9b59703f55eeea
Pashan Irani <pashanirani@gmail.com>
2017-11-18 09:23:44 -0800

    removed logs

--- controllers/mysqli_connect.php
+++ controllers/mysqli_connect.php
@@ -7,11 +7,6 @@ define('DB_PASS', $url["pass"]);
 define('DB_HOST', $url["host"]);
 define('DB_NAME', substr($url["path"], 1));
 
-error_log(DB_HOST);
-error_log(DB_USER);
-error_log(DB_PASS);
-error_log(DB_NAME);
-
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if (!$dbc) {
